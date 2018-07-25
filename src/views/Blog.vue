d918d2a9c1a00e287df1ad74d4b33fcbe7991f0a
Marco Poletto <marco.poletto@magairports.com>
2018-07-25 10:31:00 +0100

    Added firebase based blog

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,8 +1,48 @@
 <template>
-    <div class="container">
-        <h1>Coming soon</h1>
+    <div class="blog-cat__home">
+        <h1>Blog</h1>
+        <h2 v-if="loading">Please wait while the contentg is loading...</h2>
+        <article v-if="!loading" v-for="(article, index) in articles" :key="index" class="blog-cat__post">
+            <div class="blog-cat__image">
+                <img :src="article.image" alt="" class="blog-cat__image">
+            </div>
+            <div class="blog-cat__title">
+                <h2 >{{article.title}}</h2>
+            </div>
+            <div class="blog-cat__text">
+                <p>{{article.content}}</p>
+            </div>
+        </article>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            loading: true,
+            articles: []
+        }
+    },
+    methods: {
+        
+        
+    },
+    created() {
+        fetch('https://marcopolettouk.firebaseio.com/articles/.json')
+        .then(res => {
+            return res.json();
+        })
+        .then(res => {
+            console.log(res);
+            this.articles = res;
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+            this.loading = false;
+        })
+    }
+}
+</script>
 
 <style lang="scss" scoped>
     .container {
